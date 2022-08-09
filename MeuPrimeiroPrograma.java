fd0e84af833b1ee8a3225a29fc60182f7e334062
Gabriel <100156732+gameplaybiel@users.noreply.github.com>
2022-08-09 09:54:48 -0300

    MeuPrimeiroPrograma
    Programação em Java

---
+++ MeuPrimeiroPrograma.java
@@ -0,0 +1,14 @@
+public class MeuPrimeiroPrograma {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+        int a = 65;
+        int b = 75;
+        int soma = a + b;
+
+        System.out.println(soma);
+        System.out.println("Fim do cálculo, problema resolvido");
+    }
+} 
